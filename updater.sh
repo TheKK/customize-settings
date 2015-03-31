c21bc8d7c05df4c328775b9f685c3f74ab8996fa
TheKK <thumbd03803@gmail.com>
2015-03-31 15:01:34 +0800

    Enhance updater script

--- updater.sh
+++ updater.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
+echo "Start update process..."
+
 cp -r ~/.vim/colors ./
 cp -r ~/.vimrc ./
 cp -r ~/.zshrc ./
 cp -r ~/.i3 ./
 cp -r ~/.i3status.conf ./
+
+git diff
+
+echo "Done! Jolly good!!"
+exit 0
